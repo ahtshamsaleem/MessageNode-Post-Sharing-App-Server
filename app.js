2bfa61d8a021f250146f50da1bacad4ccaf264b8
Ihtisham Ul Haq <132603691+ahtshamsaleem@users.noreply.github.com>
2024-02-12 17:45:55 +0500

    Update app.js

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ mongoose
     )
     .then((result) => {
         console.log('connected to db');
-        app.listen(process.env.NODE_ENV || 8080);
+        app.listen(process.env.PORT || 8080);
     })
     .catch((err) => console.log(err));
